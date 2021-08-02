8ccd5e6040a0adf88fbf4124ebc31f6cca9bca1b
zvenko <lucianmg05@gmail.com>
2021-08-02 10:53:12 +0300

    finished responsive nav and hero

--- components/homepage/hero/hero.js
+++ components/homepage/hero/hero.js
@@ -2,28 +2,36 @@ import useTranslate from 'next-translate/useTranslation';
 import Image from 'next/image';
 
 import classes from '/styles/hero.module.css';
+import IconStudent from '../../ui/icons/icon-student';
+import IconVolunteer from '../../ui/icons/icon-volunteer';
 
 const Hero = () => {
   const { t } = useTranslate();
 
   return (
-    <section className={classes.Hero}>
-      <div className={classes.HeroImage}>
-        <Image src={'/images/profile-picture.jpg'} alt={'Imagine cu Manea Lucian George.'} width={400} height={400} quality={100} />
-      </div>
-      <div className={classes.HeroTitle}>
-        <h1>
-          <strong>{t('common:heroText')}</strong>
-        </h1>
-        <Image src={'/images/underline.svg'} width={170} height={8} />
-      </div>
-      <em className={classes.HeroDescription}>{t('common:heroDescription')}</em>
-      <div className={classes.HeroPersonalTitles}>
-        <div>
-          <p>{t('common:heroPersonalTitle1')}</p>
+    <section className={classes.HeroContainer}>
+      <div className={classes.Hero}>
+        <div className={classes.HeroImage}>
+          <Image src={'/images/profile-picture.jpg'} alt={'Imagine cu Manea Lucian George.'} width={400} height={400} quality={100} />
         </div>
-        <div>
-          <p>{t('common:heroPersonalTitle2')}</p>
+        <div className={classes.HeroTitle}>
+          <h1>
+            <strong>{t('common:heroText')}</strong>
+          </h1>
+          <Image src={'/images/underline.svg'} width={170} height={8} />
+        </div>
+        <em className={classes.HeroDescription}>{t('common:heroDescription')}</em>
+        <div className={classes.HeroPersonalTitles}>
+          <div className={classes.HeroPersonalTitlesContent1}>
+            <IconStudent />
+            <p>{t('common:heroPersonalTitle1')}</p>
+          </div>
+          <div className={classes.HeroPersonalTitlesContent2}>
+            <IconVolunteer />
+            <p>{t('common:heroPersonalTitle2')}</p>
+          </div>
+          <div className={classes.HeroPersonalTitlesBackground1}></div>
+          <div className={classes.HeroPersonalTitlesBackground2}></div>
         </div>
       </div>
     </section>
---
+++ components/ui/icons/icon-calendar.js
@@ -0,0 +1,67 @@
+const IconCalendar = () => {
+  return (
+    <svg xmlns='http://www.w3.org/2000/svg' xmlnsXlink='http://www.w3.org/1999/xlink' width='44' height='44' viewBox='0 0 640 640'>
+      <defs>
+        <path id='le9BKzNGI' d='M237.03 265.01c-13.08 0-23.7 10.61-23.7 23.7v23.7c0 13.09 10.62 23.7 23.7 23.7h47.41c13.1 0 23.71-10.61 23.71-23.7v-23.7c0-13.09-10.61-23.7-23.71-23.7h-47.41z'></path>
+        <path id='ag0xXPtVU' d='M355.56 265.01c-13.1 0-23.71 10.62-23.71 23.7v23.7c0 13.09 10.61 23.7 23.71 23.7h47.41c13.09 0 23.7-10.61 23.7-23.7v-23.7c0-13.09-10.61-23.7-23.7-23.7h-47.41z'></path>
+        <path id='f7XxUhtNA' d='M474.08 265.01c-13.1 0-23.71 10.62-23.71 23.7v23.7c0 13.09 10.61 23.7 23.71 23.7h47.4c13.09 0 23.71-10.61 23.71-23.7v-23.7c0-13.09-10.62-23.7-23.71-23.7h-47.4z'></path>
+        <path id='eXPTmu0Ie' d='M118.51 367.41c-13.09 0-23.7 10.61-23.7 23.7v23.7c0 13.09 10.61 23.71 23.7 23.71h47.41c13.1 0 23.7-10.62 23.7-23.71v-23.7c.01-13.09-10.6-23.7-23.7-23.7h-47.41z'></path>
+        <path id='c4pOekIUee' d='M237.03 367.41c-13.08 0-23.7 10.61-23.7 23.7v23.7c0 13.09 10.62 23.71 23.7 23.71h47.41c13.1 0 23.71-10.62 23.71-23.71v-23.7c0-13.09-10.61-23.7-23.71-23.7h-47.41z'></path>
+        <path id='b3Tyw7FU' d='M355.56 367.41c-13.1 0-23.71 10.61-23.71 23.7v23.7c0 13.09 10.61 23.71 23.71 23.71h47.41c13.09 0 23.7-10.62 23.7-23.71v-23.7c0-13.09-10.61-23.7-23.7-23.7h-47.41z'></path>
+        <path id='a2IM87Dq8' d='M474.08 367.41c-13.1 0-23.71 10.61-23.71 23.7v23.7c0 13.09 10.61 23.71 23.71 23.71h47.4c13.09 0 23.71-10.62 23.71-23.71v-23.7c0-13.09-10.62-23.7-23.71-23.7h-47.4z'></path>
+        <path id='bebpxmONc' d='M118.51 469.8c-13.09 0-23.7 10.62-23.7 23.71v23.7c0 13.09 10.61 23.7 23.7 23.7h47.41c13.1 0 23.7-10.61 23.7-23.7v-23.7c.01-13.08-10.6-23.71-23.7-23.71h-47.41z'></path>
+        <path id='a6JHKOtdhZ' d='M237.03 469.8c-13.08 0-23.7 10.63-23.7 23.71v23.7c0 13.09 10.62 23.7 23.7 23.7h47.41c13.1 0 23.71-10.61 23.71-23.7v-23.7c0-13.08-10.61-23.71-23.71-23.71h-47.41z'></path>
+        <path id='d5qsmJgm8' d='M355.56 469.8c-13.1 0-23.71 10.62-23.71 23.71v23.7c0 13.09 10.61 23.7 23.71 23.7h47.41c13.09 0 23.7-10.61 23.7-23.7v-23.7c0-13.08-10.61-23.71-23.7-23.71h-47.41z'></path>
+        <path
+          id='d9dnvLI8W'
+          d='M539.26 75.38v35.55c0 29.41-23.92 53.34-53.34 53.34-29.4 0-53.33-23.93-53.33-53.34V75.38h-59.26v35.55c0 29.41-23.92 53.34-53.33 53.34-29.4 0-53.33-23.93-53.33-53.34V75.38h-59.26v35.55c0 29.41-23.92 53.34-53.33 53.34-29.41 0-53.34-23.93-53.34-53.34V75.38H71.11C31.9 75.38 0 107.28 0 146.49v418.13c0 39.21 31.9 71.11 71.11 71.11h497.78c39.21 0 71.11-31.9 71.11-71.11V146.49c0-39.21-31.9-71.11-71.11-71.11h-29.63zm29.63 512.94H71.11c-13.07 0-23.7-10.63-23.7-23.7V241.3c0-13.09 10.62-23.7 23.7-23.7h497.78c13.09 0 23.7 10.61 23.7 23.7v323.32c-7.09 15.8-14.99 23.7-23.7 23.7z'></path>
+        <path id='axGtPMeD4' d='M189.63 110.93V39.82c0-19.64-15.91-35.55-35.55-35.55-19.64 0-35.56 15.91-35.56 35.55v71.11c0 19.64 15.92 35.56 35.56 35.56s35.55-15.92 35.55-35.56z'></path>
+        <path id='d1ZgxWEamB' d='M355.56 110.93V39.82c0-19.64-15.92-35.55-35.56-35.55-19.63 0-35.56 15.91-35.56 35.55v71.11c0 19.64 15.93 35.56 35.56 35.56 19.64 0 35.56-15.92 35.56-35.56z'></path>
+        <path id='dyFJiOuLd' d='M521.48 110.93V39.82c0-19.64-15.92-35.55-35.56-35.55-19.63 0-35.55 15.91-35.55 35.55v71.11c0 19.64 15.92 35.56 35.55 35.56 19.64 0 35.56-15.92 35.56-35.56z'></path>
+      </defs>
+      <use xlinkHref='#le9BKzNGI'></use>
+      <use fillOpacity='0' stroke='#000' strokeOpacity='0' xlinkHref='#le9BKzNGI'></use>
+      <use xlinkHref='#ag0xXPtVU'></use>
+      <use fillOpacity='0' stroke='#000' strokeOpacity='0' xlinkHref='#ag0xXPtVU'></use>
+      <use xlinkHref='#f7XxUhtNA'></use>
+      <use fillOpacity='0' stroke='#000' strokeOpacity='0' xlinkHref='#f7XxUhtNA'></use>
+      <use xlinkHref='#eXPTmu0Ie'></use>
+      <use fillOpacity='0' stroke='#000' strokeOpacity='0' xlinkHref='#eXPTmu0Ie'></use>
+      <use xlinkHref='#c4pOekIUee'></use>
+      <use fillOpacity='0' stroke='#000' strokeOpacity='0' xlinkHref='#c4pOekIUee'></use>
+      <use xlinkHref='#b3Tyw7FU'></use>
+      <use fillOpacity='0' stroke='#000' strokeOpacity='0' xlinkHref='#b3Tyw7FU'></use>
+      <use xlinkHref='#a2IM87Dq8'></use>
+      <use fillOpacity='0' stroke='#000' strokeOpacity='0' xlinkHref='#a2IM87Dq8'></use>
+      <g>
+        <use xlinkHref='#bebpxmONc'></use>
+        <use fillOpacity='0' stroke='#000' strokeOpacity='0' xlinkHref='#bebpxmONc'></use>
+      </g>
+      <g>
+        <use xlinkHref='#a6JHKOtdhZ'></use>
+        <use fillOpacity='0' stroke='#000' strokeOpacity='0' xlinkHref='#a6JHKOtdhZ'></use>
+      </g>
+      <g>
+        <use xlinkHref='#d5qsmJgm8'></use>
+        <use fillOpacity='0' stroke='#000' strokeOpacity='0' xlinkHref='#d5qsmJgm8'></use>
+      </g>
+      <g>
+        <use xlinkHref='#d9dnvLI8W'></use>
+        <use fillOpacity='0' stroke='#000' strokeOpacity='0' xlinkHref='#d9dnvLI8W'></use>
+      </g>
+      <g>
+        <use xlinkHref='#axGtPMeD4'></use>
+        <use fillOpacity='0' stroke='#000' strokeOpacity='0' xlinkHref='#axGtPMeD4'></use>
+      </g>
+      <g>
+        <use xlinkHref='#d1ZgxWEamB'></use>
+        <use fillOpacity='0' stroke='#000' strokeOpacity='0' xlinkHref='#d1ZgxWEamB'></use>
+      </g>
+      <g>
+        <use xlinkHref='#dyFJiOuLd'></use>
+        <use fillOpacity='0' stroke='#000' strokeOpacity='0' xlinkHref='#dyFJiOuLd'></use>
+      </g>
+    </svg>
+  );
+};
+export default IconCalendar;
